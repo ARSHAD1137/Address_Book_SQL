95c9569e02bb548357436636e1ecee8402832b16
Arshad <arshadpinjari666@gmail.com>
2021-06-05 19:32:02 +0530

    Added.IdentifyUsingType

--- AddressBook_Sql.sql
+++ AddressBook_Sql.sql
@@ -17,3 +17,29 @@ create table Address_book
  email varchar(100) not null
 );
 select * from Address_book;
+
+Insert into Address_book values
+('Ajay','Singhaniya','Down city','Mumbai','Maharshtra','442111',818181881,'ajay@hotmail.com'),
+('Vijay','kadia','Lower parel','Agar','MP','412121',919191991,'vijay@gmail.com'),
+('Jack','Potter','Robert street','New york','America','231312',412112121,'jack@hotmail.com'),
+('Suresh','Agrwal','Street 40','Mumbai','Maharashtra','432121',818181818,'suresh@gmail.com'),
+('Rakesh','Patodia','High Tower','Noida','Delhi','421211',717171717,'rakesh@outlook.com');
+
+Update Address_book
+set city='Pune' where firstname='Ajay'
+
+Delete from Address_book Where firstname='Suresh'
+
+select firstname from Address_book Where city='Pune'
+select firstname from Address_book where state='Maharashtra'
+
+select COUNT (city)'Size of column' from Address_book
+select COUNT (state)'Size of column' from Address_book
+
+select *from Address_book Where city='Pune'
+Order by firstname
+
+alter table Address_book add Book_Name varchar(20),Book_Type varchar(20)
+Update Address_book set Book_Name='Address1',Book_Type='Family' where firstname='Ramesh'
+Update Address_book set Book_Name='Address2',Book_Type='Friends' where firstname='Vijay'
+Update Address_book set Book_Name='Address3',Book_Type='Professional' where firstname='Jack'
