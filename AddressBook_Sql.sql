e79340786e2c596151e095ce5865df57bd25dc45
Arshad <arshadpinjari666@gmail.com>
2021-06-05 18:27:35 +0530

    Added.RetreiveData

--- AddressBook_Sql.sql
+++ AddressBook_Sql.sql
@@ -17,3 +17,18 @@ create table Address_book
  email varchar(100) not null
 );
 select * from Address_book;
+
+Insert into Address_book values
+('Ajay','Singhaniya','Down city','Mumbai','Maharshtra','442111',818181881,'ajay@hotmail.com'),
+('Vijay','kadia','Lower parel','Agar','MP','412121',919191991,'vijay@gmail.com'),
+('Jack','Potter','Robert street','New york','America','231312',412112121,'jack@hotmail.com'),
+('Suresh','Agrwal','Street 40','Mumbai','Maharashtra','432121',818181818,'suresh@gmail.com'),
+('Rakesh','Patodia','High Tower','Noida','Delhi','421211',717171717,'rakesh@outlook.com');
+
+Update Address_book
+set city='Pune' where firstname='Ajay'
+
+Delete from Address_book Where firstname='Suresh'
+
+select firstname from Address_book Where city='Pune'
+select firstname from Address_book where state='Maharashtra'
